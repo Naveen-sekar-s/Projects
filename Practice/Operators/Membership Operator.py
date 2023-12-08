fd1d92df7328361ab95e5d672170e466e34c8733
Naveen-sekar-s <naveensekar369@gmail.com>
2023-12-08 10:29:22 +0530

    tst

---
+++ Practice/Operators/Membership Operator.py
@@ -0,0 +1,9 @@
+""" To verify element present in sequence in (or) not in.
+    str , list , tuple , set , dict.
+    eg:  N in "Naveen" """
+
+
+ids = [101,102,103,104]
+
+print(101 in ids)  #true
+print(100 in ids)  #false
