5596fc548355346d4ae60c393903e4082f3b5a6b
Naveen-sekar-s <naveensekar369@gmail.com>
2023-12-14 11:48:13 +0530

    tst

---
+++ Practice/List/listMethods/copy.py
@@ -0,0 +1,6 @@
+#print list using list copy method
+#copy()copy the list and create a new copied list
+empnames=['Dinesh','Elizabath','Farzila','Balaji','Charan','Arun']
+newlist=empnames.copy()
+print(empnames)
+print(newlist)
