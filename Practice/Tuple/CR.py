c386ffc57cde341956f1aa2d5ab982a54402583c
Naveen-sekar-s <naveensekar369@gmail.com>
2023-12-18 12:04:54 +0530

    tst

---
+++ Practice/Tuple/CR.py
@@ -0,0 +1,16 @@
+#create a tuple
+t=(10,20,30,40,50,60)
+
+#read a tuple
+print(t)
+
+#print specific value in tuple using indexing
+print(t[0])
+print(t[1])
+print(t[2])
+print(t[3])
+print(t[4])
+print(t[5])
+
+
+
---
+++ Practice/Tuple/while.py
@@ -0,0 +1,8 @@
+#print tuple values using while loop 
+t=(10,20,30,40,50,60)
+
+i=0
+while i<= len(t)-1:
+    print(t[i])
+    i=i+1
+    
