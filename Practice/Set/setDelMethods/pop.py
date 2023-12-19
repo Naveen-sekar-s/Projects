5100f6741a0c18f5a4b8368d1c29d6ae52afcfbd
Naveen-sekar-s <naveensekar369@gmail.com>
2023-12-19 11:37:22 +0530

    tst

---
+++ Practice/Set/setDelMethods/pop.py
@@ -0,0 +1,11 @@
+#indexing are not there so pop method is not a correct way
+#bcoz it deletes the last elements randomly
+#eg(1)
+a1={1000,2000,3000,4000}
+a1.pop()
+print(a1)
+
+#eg(2)
+enames={"Ragul","Sonia","Reddy","Priyanka","Mandela","Modi"}
+enames.pop()
+print(enames)
